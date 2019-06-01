72bf3ffaba9242a688024e3b00c1dd0ebd79014d
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-01 17:41:36 -0600

    Adds citycurrent model

--- app/models/city_current.rb
+++ app/models/city_current.rb
@@ -1,2 +1,13 @@
 class CityCurrent < ApplicationRecord
+  validates_presence_of :temp,
+                        :apparent,
+                        :icon,
+                        :cloud_cover,
+                        :humidity,
+                        :visibility,
+                        :uv_index,
+                        :wind_speed,
+                        :wind_direction
+
+  belongs_to :city
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_231612) do
+ActiveRecord::Schema.define(version: 2019_06_01_233840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 2019_06_01_231612) do
     t.string "state"
     t.string "latitude"
     t.string "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "city_currents", force: :cascade do |t|
+    t.decimal "temp"
+    t.decimal "apparent"
+    t.string "icon"
+    t.decimal "cloud_cover"
+    t.decimal "humidity"
+    t.decimal "visibility"
+    t.decimal "uv_index"
+    t.decimal "wind_speed"
+    t.decimal "wind_direction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
