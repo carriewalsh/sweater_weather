8f20099730c428136a4abc2cf3414067079e972b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-01 17:25:40 -0600

    Sets up gems and rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace 'api' do
-    namespacae 'v1' do
+    namespace 'v1' do
       get '/forecast', to: 'forecast#show'
+
+      # get '/register', to: 'users#new'
+      # post '/users', to: 'users#create'
+
+      # get '/login', to: 'sessions#new', as: login
+      # post '/login', to: 'sessions#create'
+      # post '/logout', to: 'sessions#destroy', as: logout
     end
   end
 end
