50df43f693a737e8ad01790a516e666aef813a62
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-01 21:48:59 -0600

    Adds before each to city day service spec

--- spec/services/city_day_service_spec.rb
+++ spec/services/city_day_service_spec.rb
@@ -3,10 +3,17 @@ require "rails_helper"
 describe CityDayService, type: :service do
   before :each do
     @city = City.create(name: "Salem", state: "Oregon", latitude: 44.07, longitude: -123, photo_url: "a url")
-    @city_day_service = SteadyService.new(@city)
-    url = "https://weather.cit.api.here.com/weather/1.0/report.json?name=salem oregon&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&product=forecast_astronomy"
+    @city_day_service = CityDayService.new(@city)
+    geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@city.name + ' ' + @city.state}n&key=#{ENV['GOOGLE_SECRET_KEY']}"
+    geo_results = JSON.parse(actual.body, symbolize_names: true)[:results][:geometry][:location]
+    lat = geo_results[:lat]
+    long = geo_results[:lng]
+
+    weather_url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_SECRET_KEY']}/#{lat},#{long}"
+    binding.pry
     actual = Faraday.get(url)
     @body = JSON.parse(actual.body, symbolize_names: true)[:astronomy][:astronomy]
+    ENV['DARK_SKY_SECRET_KEY']
   end
 
   describe "instance methods" do
