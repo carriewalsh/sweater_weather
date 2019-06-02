564efdf7efa0aec367bf5b2d921426792215cd96
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-02 09:41:05 -0600

    Adds information to forecast api get show

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     city = CityCreatorService.new(city_params).find_or_create_city
+    current = CityCurrent.where(city_id: city.id)
+    astronomy = CitySteady.where(city_id: city.id)
+    forecast = CityDay.where(city_id: city.id)
   end
 
   private
