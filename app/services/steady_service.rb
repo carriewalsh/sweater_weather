3fa0628903ef8f0a165c631c083434fcba212a09
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-01 21:20:33 -0600

    Adds spec and method for create_or_update steady

--- app/services/steady_service.rb
+++ app/services/steady_service.rb
@@ -9,8 +9,14 @@ class SteadyService
   end
 
   def create_or_update
-    if CitySteady.find_by(city_id: @city.id)
+    days = CitySteady.where(city_id: @city.id)
+    unless days.empty?
+      data = get_astros
+      data.each do |datum|
+        update(datum)
+      end
     else
+      create
     end
   end
 
@@ -24,5 +30,27 @@ class SteadyService
     def get_json
       response = conn.body
       JSON.parse(response, symbolize_names: true)
+    end
+
+    def create
+      data = get_astros
+      data.each do |datum|
+        @city.city_steadies.create(day_id: Day.find_by(
+          date: datum[:utcTime].to_date).id,
+          sunrise: datum[:sunrise],
+          sunset: datum[:sunset],
+          moon_phase: datum[:moonPhase],
+          phase_description: datum[:moonPhaseDesc],
+          phase_icon: datum[:iconName])
+      end
+    end
+
+    def update(datum)
+      steady = @city.city_steadies.where(day_id: Day.find_by(date: datum[:utcTime].to_date).id)
+      steady.update(sunrise: datum[:sunrise],
+                    sunset: datum[:sunset],
+                    moon_phase: datum[:moonPhase],
+                    phase_description: datum[:moonPhaseDesc],
+                    phase_icon: datum[:iconName])
     end
 end
--- spec/services/steady_service_spec.rb
+++ spec/services/steady_service_spec.rb
@@ -2,18 +2,12 @@ require "rails_helper"
 
 describe SteadyService, type: :service do
   before :each do
-    @city = City.new(name: "Salem", state: "Oregon", latitude: 44.07, longitude: -123)
+    @city = City.create(name: "Salem", state: "Oregon", latitude: 44.07, longitude: -123, photo_url: "a url")
     @steady_service = SteadyService.new(@city)
     url = "https://weather.cit.api.here.com/weather/1.0/report.json?name=salem oregon&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&product=forecast_astronomy"
     actual = Faraday.get(url)
     @body = JSON.parse(actual.body, symbolize_names: true)[:astronomy][:astronomy]
-    Day.create(name: "Sunday", abbreviation: "Sun", date: Date.today)
-    Day.create(name: "Monday", abbreviation: "Mon", date: Date.today)
-    Day.create(name: "Tuesday", abbreviation: "Tue", date: Date.today)
-    Day.create(name: "Wednesday", abbreviation: "Wed", date: Date.today)
-    Day.create(name: "Thursday", abbreviation: "Thu", date: Date.today)
-    Day.create(name: "Friday", abbreviation: "Fri", date: Date.today)
-    Day.create(name: "Saturday", abbreviation: "Sat", date: Date.today)
+
   end
   describe 'Instance methods' do
     describe "get_astros" do
@@ -30,13 +24,44 @@ describe SteadyService, type: :service do
     end
 
     describe "create_or_update" do
-      xit "creates or updates city_steadies in db" do
+      it "creates city_steadies in db" do
+        today = Date.today
+        count = 0
+        while count < 7
+          day = (today + count.days)
+          Day.create(name: day.strftime("%B"), abbreviation: day.strftime("%b"), date: day)
+          count += 1
+        end
+        expect(CitySteady.count).to eq(0)
+
         @steady_service.create_or_update
-        today =
-        steadies = CitySteady.where(city_id: @city.id)
         expect(CitySteady.count).to eq(7)
-        expect()
-        # this should create or update 7 city_steadies for that city and that week by calling get_astros on that city
+        expect(CitySteady.first.sunrise).to eq(@body.first[:sunrise])
+        expect(CitySteady.last.sunrise).to eq(@body.last[:sunrise])
+      end
+
+      it "creates city_steadies in db" do
+        today = Date.today
+        count = 0
+        while count < 7
+          day = (today + count.days)
+          Day.create(name: day.strftime("%B"), abbreviation: day.strftime("%b"), date: day)
+          CitySteady.create(city_id: @city.id, day_id: Day.find_by(date: day).id,
+          sunrise: "1",
+          sunset: "2",
+          moon_phase: 3,
+          phase_description: "blah",
+          phase_icon: "blah")
+          count += 1
+        end
+        expect(CitySteady.count).to eq(7)
+        expect(CitySteady.first.sunrise).to eq("1")
+        expect(CitySteady.last.sunrise).to eq("1")
+
+        @steady_service.create_or_update
+        expect(CitySteady.count).to eq(7)
+        expect(CitySteady.first.sunrise).to eq(@body.first[:sunrise])
+        expect(CitySteady.last.sunrise).to eq(@body[6][:sunrise])
       end
     end
   end
