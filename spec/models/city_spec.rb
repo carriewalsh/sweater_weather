6881e84fef420f658ce6d4bdfb681e25a83a751e
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-02 08:03:14 -0600

    Adds tests for instance methods

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -14,4 +14,23 @@ describe City, type: :model do
     it { should validate_presence_of :longitude }
     it { should validate_presence_of :photo_url }
   end
+
+  describe "Instance methods" do
+    before :each do
+      @city = City.create(name: "Salem", state: "Oregon", latitude: "44.07", longitude:" -123", photo_url: "a url")
+    end
+    describe "coordinates" do
+      it "should return a string of both lat/long coordinates" do
+        result = @city.coordinates
+        expect(result).to eq("44.07,-123")
+      end
+    end
+
+    describe "name_string" do
+      it "should return a string of the city and state" do
+        result = @city.name_string
+        expect(result).to eq("Salem, Oregon")
+      end
+    end
+  end
 end
