7b27b131432ceb59b6fa7bfed883dbd2e029d84c
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-01 22:31:03 -0600

    Adds spec for latlong service

--- app/services/city_day_service.rb
+++ app/services/city_day_service.rb
@@ -3,4 +3,15 @@ class CityDayService
   def initialize(city)
     @city = city
   end
+
+  def get_forecast
+
+  end
+
+
+  private
+
+    def conn
+
+    end
 end
---
+++ spec/services/lat_long_service_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe LatLongService, type: :service do
+  before :each do
+    @city = City.create(name: "Salem", state: "Oregon", latitude: 44.07, longitude: -123, photo_url: "a url")
+    @lat_long_service = LatLongService.new(@city)
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@city.name + ' ' + @city.state}n&key=#{ENV['GOOGLE_SECRET_KEY']}"
+    actual_geo = Faraday.get(url)
+    results = JSON.parse(actual_geo.body, symbolize_names: true)[:results].first[:geometry][:location]
+    @lat = results[:lat]
+    @long = results[:lng]
+  end
+
+  describe "instance methods" do
+    describe "get_lat_long" do
+      it "gets the latitude and longitude for a location" do
+        result = @lat_long_service.get_lat_long
+        expect(result.lat).to eq(@lat)
+        expect(result.long).to eq(@long)
+      end
+    end
+
+    describe "combine" do
+      it "combines lat and long for DarkSky search" do
+        result = @lat_long_service.combine
+        expect(result).to eq("#{@lat},#{@long}")
+      end
+    end
+  end
+
+end
