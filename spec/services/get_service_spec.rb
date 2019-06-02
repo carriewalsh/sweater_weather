76d8e6a29b462ca4f20d113a11c15603e95fe6e5
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-02 09:05:15 -0600

    Combines tests into one

---
+++ spec/services/get_service_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+describe "GetService", type: :module do
+  describe "Instance methods" do
+    before :each do
+      @city = City.create(name: "Salem", state: "Oregon", latitude: "44.07", longitude: "-123", photo_url: "a url")
+      @steady_service = SteadyService.new(@city)
+      @city_day_service = CityDayService.new(@city)
+      @current_service = CurrentService.new(@city)
+    end
+
+    describe "conn and get_json" do
+      it "should successfully send a get request with a given url and parse it" do
+        astronomy_body = @steady_service.get_json
+        forecast_body = @city_day_service.get_json
+        current_body = @current_service.get_json
+
+        astronomy_expected = astronomy_body[:astronomy][:astronomy].first
+        expect(astronomy_expected).to be_a(Hash)
+        expect(astronomy_expected.include?(:sunrise)).to be true
+        expect(astronomy_expected.include?(:moonPhase)).to be true
+
+        forecast_expected = forecast_body[:daily][:data].first
+        expect(forecast_expected).to be_a(Hash)
+        expect(forecast_expected.include?(:summary))
+
+        current_expected = current_body[:currently]
+        expect(current_expected).to be_a(Hash)
+        expect(current_expected.include?(:summary)).to be true
+        expect(current_expected.include?(:temperature)).to be true
+      end
+    end
+  end
+end
