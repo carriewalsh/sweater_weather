53fea82832bf1699c5a480a506f3e7a74720b96f
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-02 00:20:41 -0600

    Adds spec for city creator service

---
+++ spec/services/city_creator_service_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe CityCreatorService, type: :service do
+  describe "Instance methods" do
+    describe "create_city" do
+      it "creates a city and state for latitude/longitude given" do
+        input = "Salem Oregon"
+        expect(City.count).to eq(0)
+        coordinates = LatLongService.new(input).combine
+        city_creator_service = CityCreatorService.new(coordinates)
+        city_creator_service.create_city
+        expect(City.first.name).to eq("Salem")
+        expect(City.first.state).to eq("Oregon")
+        expect(City.first.latitude).to eq(coordinates.split[0])
+        expect(City.first.longitude).to eq(coordinates.split[1])
+      end
+    end
+  end
+end
