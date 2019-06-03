8405b3c258667fffc11767cad0a6ba7ed1e355f0
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-03 09:54:16 -0600

    Finishes antipode part of request spec

--- spec/requests/user_can_find_antipodes_spec.rb
+++ spec/requests/user_can_find_antipodes_spec.rb
@@ -3,14 +3,21 @@ require "rails_helper"
 describe "As a visitor", type: :request do
   describe "When I enter a city" do
     it "should return my weather and the weather of my antipode" do
-      city = CityCreatorService.new("rio de janero").find_or_create_city
-      city2 = CityCreatorService.new("Paris, France").find_or_create_city
-      city1 = CityCreatorService.new("Buenos Aires Argentina").find_or_create_city
+      city = CityCreatorService.new("Hong Kong").find_or_create_city
       lat = city.latitude
       long = city.longitude
-      binding.pry
 
-      Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=27&long=-82")
+      conn = Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{lat}&long=#{long}") do |f|
+        f.headers["api_key"] = ENV["AMY_KEY"]
+        f.adapter Faraday.default_adapter
+      end
+      body = conn.get.body
+      data = JSON.parse(body, symbolize_names: true)[:data]
+
+      expect(data[:type]).to eq("antipode")
+      expect(data[:attributes].keys.include?(:lat)).to be true
+      expect(data[:attributes].keys.include?(:long)).to be true
+
     end
   end
 end
