ebe9192a07b889edee28f35417a2a261a63e7bdc
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-02 20:33:26 -0600

    Adds simplecov, rake, and webmock to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,21 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+require 'webmock/rspec'
+require 'rake'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+SimpleCov.start 'rails' do
+  add_filter '/channels/'
+  add_filter '/jobs/'
+  add_filter '/mailers/'
+  add_filter '/helpers/'
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
