4ec0d9c63818987a51760974226302f9c5f6834b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-04 13:01:45 -0600

    Dries up favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,42 +1,39 @@
 class Api::V1::FavoritesController < ApplicationController
   protect_from_forgery with: :null_session
+  before_action :user?
 
   def create
-    user = User.find_by(api_key: favorite_params[:api_key])
     city = City.find(favorite_params[:location])
     current = CityCurrent.find_by(city_id: city)
-    if user && city
-      UserCity.create!(user_id: user.id, city_id: city.id, city_current_id: current.id)
+    if city
+      UserCity.create!(user_id: @user.id, city_id: city.id, city_current_id: current.id)
       render json: { success: "Favorited" }, status: 200
-    else
-      render json: { error: "Unauthorized" }, status: 401
     end
   end
 
   def show
-    user = User.find_by(api_key: favorite_params[:api_key])
-    if user
-      favorites = user.cities
-      render json: UserCitySerializer.new(user,favorites).jsonify, status: 200
-    else
-      render json: { error: "Unauthorized" }, status: 401
-    end
+    favorites = @user.cities
+    render json: UserCitySerializer.new(@user,favorites).jsonify, status: 200
   end
 
   def destroy
-    user = User.find_by(api_key: favorite_params[:api_key])
-    if user
       UserCity.find_by(city_id: favorite_params[:location]).destroy
-      favorites = user.cities
-      render json: UserCitySerializer.new(user,favorites).jsonify, status: 200
-    else
-      render json: { error: "Unauthorized" }, status: 401
-    end
+      favorites = @user.cities
+      render json: UserCitySerializer.new(@user,favorites).jsonify, status: 200
   end
 
   private
 
     def favorite_params
       params.permit(:location, :api_key)
+    end
+
+    def user?
+      @user = User.find_by(api_key: favorite_params[:api_key])
+      unless @user
+        render json: { error: "Unauthorized" }, status: 401
+      else
+        @user
+      end
     end
 end
