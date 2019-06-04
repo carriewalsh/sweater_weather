3cabbdd27e7ab358f4a22fbb9d9178eee43c92fe
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-04 10:54:49 -0600

    Removes private antipode method from spec - 100% testing

--- spec/services/antipode_service_spec.rb
+++ spec/services/antipode_service_spec.rb
@@ -2,17 +2,6 @@ require "rails_helper"
 
 describe AntipodeService, type: :service do
   describe "instance methods" do
-    describe "get_antipode" do
-      xit "gets the antipode lat/long" do
-        input = "hongkong"
-
-        data = AntipodeService.new(input).get_antipode
-        expect(data[:type]).to eq("antipode")
-        expect(data[:attributes].keys.include?(:lat)).to be true
-        expect(data[:attributes].keys.include?(:long)).to be true
-      end
-    end
-
     describe "create_or_find_city" do
       it "creates antipode city" do
         AntipodeService.new("hongkong").create_or_find_city
