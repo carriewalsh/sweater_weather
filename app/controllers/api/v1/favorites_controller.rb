b8b47ebab4b0c4e66da270b3ad850241d61eeddc
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-03 21:17:39 -0600

    Adds foregry null session to sessions controller and (new) favorites controller

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::FavoritesController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
