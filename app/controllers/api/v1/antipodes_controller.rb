9b4cc0f5d1e0958a2b72ad9c9667c32a94c5047a
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-04 11:56:35 -0600

    Adds forgery null session to antipodes controller

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::AntipodesController < ApplicationController
+  protect_from_forgery with: :null_session
+  
   def show
     antipode = AntipodeService.new(params[:loc]).create_antipode
     render json: AntipodeSerializer.new(antipode, include: [:city, :city_current])
