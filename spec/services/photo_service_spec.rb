8f1c45bf89f1cf2c64e30a40bd8f1aede2c91b7b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-04 13:35:51 -0600

    Adds return value to get photo method

--- spec/services/photo_service_spec.rb
+++ spec/services/photo_service_spec.rb
@@ -6,7 +6,7 @@ describe PhotoService, type: :service do
       it "gets park photo (hopefully) based on the city" do
         city = City.create(name: "Salem", state: "Oregon", country: "United States", latitude: 44.07, longitude: -123)
         photo_service = PhotoService.new(city)
-        data = photo_service.get_json[:photos][:photo].first
+        data = photo_service.get_json(nil,nil)[:photos][:photo].first
         result = photo_service.get_photo
         expect(result).to eq(data)
       end
