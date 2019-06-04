d9737b462735d43ba3ae95640eca76f79dde7a36
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-04 11:51:28 -0600

    Changes attributes to attr_accessor in user city serializer

--- app/serializers/user_city_serializer.rb
+++ app/serializers/user_city_serializer.rb
@@ -1,7 +1,7 @@
 class UserCitySerializer
 
-  attributes :id, :city_id, :user_id, :city_current_id
-  
+  attr_accessor :id, :city_id, :user_id, :city_current_id
+
   def initialize(user, favorites)
     @user = user
     @forecasts = favorites.map { |favorite| {favorite.name => favorite.city_current} }
