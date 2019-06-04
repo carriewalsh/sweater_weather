01b3dec3be906911158dbfdb2bb93a344e7517ad
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-04 12:30:15 -0600

    Adds if statement to photo service for cities that do not have states

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -4,7 +4,11 @@ class PhotoService
 
   def initialize(city)
     @city = city
-    @url = "https://api.flickr.com/services/rest?api_key=#{ENV["FLICKR_API_KEY"]}&method=flickr.photos.search&format=json&text=park #{city.name} #{city.state}&sort=relevance&nojsoncallback=1"
+    if @city.state
+      @url = "https://api.flickr.com/services/rest?api_key=#{ENV["FLICKR_API_KEY"]}&method=flickr.photos.search&format=json&text=park #{city.name} #{city.state}&sort=relevance&nojsoncallback=1"
+    else
+      @url = "https://api.flickr.com/services/rest?api_key=#{ENV["FLICKR_API_KEY"]}&method=flickr.photos.search&format=json&text=park #{city.name} #{city.country}&sort=relevance&nojsoncallback=1"
+    end
   end
 
   def get_photo
